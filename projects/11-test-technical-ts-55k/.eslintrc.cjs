2470daa3bebdb825e40a9b01ad128d700a0a5251
Jonnathan <jonnabcl56@gmail.com>
2023-06-05 20:51:42 -0500

    Add project 11( linter config)

--- projects/11-test-technical-ts-55k/.eslintrc.cjs
+++ projects/11-test-technical-ts-55k/.eslintrc.cjs
@@ -1,19 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/method-signature-style': 'off'
+  }
 }
--- projects/11-test-technical-ts-55k/src/App.tsx
+++ projects/11-test-technical-ts-55k/src/App.tsx
@@ -1,6 +1,6 @@
 
-export function App() {
+export function App () {
   return (
     <h1>React + Ts - Technical test 55k</h1>
-  )  
+  )
 }
