15d51c14d65a85ebfe8db2d087c4571ea256dd76
Jonnathan <jonnabcl56@gmail.com>
2023-06-06 21:40:07 -0500

    fix

--- projects/11-test-technical-ts-55k/.eslintrc.cjs
+++ projects/11-test-technical-ts-55k/.eslintrc.cjs
@@ -11,6 +11,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/method-signature-style': 'off'
+    '@typescript-eslint/method-signature-style': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
   }
 }
