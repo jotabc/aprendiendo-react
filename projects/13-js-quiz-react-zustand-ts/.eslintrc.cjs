77b5c00b10cf975c32ea02bc4c4398bdff0125cb
Jonnathan <jonnabcl56@gmail.com>
2023-06-16 09:24:32 -0500

    add project 13 (Js quiz) add material UI + linter(ts-standard config)

--- projects/13-js-quiz-react-zustand-ts/.eslintrc.cjs
+++ projects/13-js-quiz-react-zustand-ts/.eslintrc.cjs
@@ -3,12 +3,18 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended'
+    'plugin:react-hooks/recommended',
+    './node_modules/ts-standard/eslintrc.json'
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn'
+    'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off'
   }
 }
